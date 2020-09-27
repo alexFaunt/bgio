bb6be056de9cfccdba68e7d30217a5fc47f1e982
alexfaunt <alexfaunt@gmail.com>
2020-09-27 22:34:13 +0100

    its late

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,8 +1,12 @@
 /* eslint-disable no-process-env */
 
+const opt = (input: string): string | null => (
+  input === 'undefined' ? null : input
+);
+
 const config = {
   VERSION: `${process.env.REACT_APP_VERSION}`,
-  SERVER_URL: `${process.env.REACT_APP_SERVER_URL}`,
+  SERVER_URL: opt(`${process.env.REACT_APP_SERVER_URL}`),
   GRAPHQL_URL: `${process.env.REACT_APP_GRAPHQL_URL}`,
   BGIO_DEBUG: process.env.REACT_APP_BGIO_DEBUG === 'true',
 };
